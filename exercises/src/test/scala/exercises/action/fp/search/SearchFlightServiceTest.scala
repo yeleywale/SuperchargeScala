8063d225746e51c4806380e1b7e5b887c5df420f
yeleywale <waleander@gmail.com>
2022-06-05 00:51:08 +0100

    Activate tests in SearchFlightServiceTest.scala to support changes in SearchFlightServiceTest.Scala

--- exercises/src/test/scala/exercises/action/fp/search/SearchFlightServiceTest.scala
+++ exercises/src/test/scala/exercises/action/fp/search/SearchFlightServiceTest.scala
@@ -4,8 +4,7 @@ import exercises.action.DateGenerator._
 import exercises.action.fp.IO
 import exercises.action.fp.search.Airport._
 import exercises.action.fp.search.SearchFlightGenerator._
-import org.scalacheck.{Arbitrary, Gen}
-import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
@@ -18,7 +17,7 @@ import scala.util.Random
 // testOnly exercises.action.fp.search.SearchFlightServiceTest
 class SearchFlightServiceTest extends AnyFunSuite with ScalaCheckDrivenPropertyChecks {
 
-  ignore("fromTwoClients example") {
+  test("fromTwoClients example") {
     val now   = Instant.now()
     val today = LocalDate.now()
 
@@ -30,10 +29,85 @@ class SearchFlightServiceTest extends AnyFunSuite with ScalaCheckDrivenPropertyC
     val client1 = SearchFlightClient.constant(IO(List(flight3, flight1)))
     val client2 = SearchFlightClient.constant(IO(List(flight2, flight4)))
 
-    val service = SearchFlightService.fromTwoClients(client1, client2)
+    val service = SearchFlightService.fromTwoClients(client1, client2)(ExecutionContext.global)
     val result  = service.search(parisOrly, londonGatwick, today).unsafeRun()
 
     assert(result == SearchResult(List(flight1, flight2, flight3, flight4)))
+  }
+
+//  test("fromTwoClients should handle error gracefully") {
+//    forAll(airportGen, airportGen, dateGen, Gen.listOf(flightGen), arbitrary[Throwable]) {
+//      (from, to, date, flights, error) =>
+//      val now   = Instant.now()
+//
+//      val flight1 = Flight("1", "BA", parisOrly, londonGatwick, now, Duration.ofMinutes(100), 0, 89.5, "")
+//
+//      val client1 = SearchFlightClient.constant(IO(flights))
+//      val client2 = SearchFlightClient.constant(IO.fail(error))
+//
+//      val service = SearchFlightService.fromTwoClients(client1, client2)
+//      val result  = service.search(from, to, date).attempt.unsafeRun()
+//
+//      assert(result.isSuccess)
+//    }
+//
+//  }
+
+  test("fromTwoClients should handle error gracefully") {
+    forAll(airportGen, airportGen, dateGen, clientGen, clientGen) {
+      (from, to, date, client1, client2) =>
+
+        val service = SearchFlightService.fromTwoClients(client1, client2)(ExecutionContext.global)
+        val result  = service.search(from, to, date).attempt.unsafeRun()
+
+        assert(result.isSuccess)
+    }
+
+  }
+
+  test("fromclients example") {
+    val now   = Instant.now()
+    val today = LocalDate.now()
+
+    val flight1 = Flight("1", "BA", parisOrly, londonGatwick, now, Duration.ofMinutes(100), 0, 89.5, "")
+    val flight2 = Flight("2", "LH", parisOrly, londonGatwick, now, Duration.ofMinutes(105), 0, 96.5, "")
+    val flight3 = Flight("3", "BA", parisOrly, londonGatwick, now, Duration.ofMinutes(140), 1, 234.0, "")
+    val flight4 = Flight("4", "LH", parisOrly, londonGatwick, now, Duration.ofMinutes(210), 2, 55.5, "")
+
+    val client1 = SearchFlightClient.constant(IO(List(flight3, flight1)))
+    val client2 = SearchFlightClient.constant(IO(List(flight2, flight4)))
+
+    val service = SearchFlightService.fromClients(List(client1, client2))(ExecutionContext.global)
+    val result  = service.search(parisOrly, londonGatwick, today).unsafeRun()
+
+    assert(result == SearchResult(List(flight1, flight2, flight3, flight4)))
+  }
+
+  test("fromClients should handle error gracefully") {
+    forAll(airportGen, airportGen, dateGen, Gen.listOf(clientGen)) {
+      (from, to, date, clients) =>
+
+        val service = SearchFlightService.fromClients(clients)(ExecutionContext.global)
+        val result  = service.search(from, to, date).attempt.unsafeRun()
+
+        assert(result.isSuccess)
+    }
+
+  }
+
+  test("fromClients - clients order doesnt matter") {
+    forAll(airportGen, airportGen, dateGen, Gen.listOf(clientGen)) {
+      (from, to, date, clients) =>
+
+        val service1 = SearchFlightService.fromClients(clients)(ExecutionContext.global)
+        val service2 = SearchFlightService.fromClients(Random.shuffle(clients))(ExecutionContext.global)
+
+        val result1 = service1.search(from, to, date).attempt.unsafeRun()
+        val result2  = service2.search(from, to, date).attempt.unsafeRun()
+
+        assert(result1 == result2)
+    }
+
   }
 
 }
